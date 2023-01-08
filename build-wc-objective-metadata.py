57a1479bdaaa91caf7da2ee7e2e291e7c86bf94d
Kiel <95580337+kielbasiago@users.noreply.github.com>
2023-01-08 15:07:45 -0500

    light cleanup

--- build-wc-objective-metadata.py
+++
@@ -1,7 +0,0 @@
-def main():
-    from metadata.objective_metadata_writer import ObjectiveMetadataWriter
-    metadata_writer = ObjectiveMetadataWriter()
-    metadata_writer.write()
-
-if __name__ == '__main__':
-    main()
